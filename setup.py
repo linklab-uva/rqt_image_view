962604160a3f41cf464136f39c2083e17cc0e4c5
Trent Weiss <ttw2xk@virginia.edu>
2019-12-02 15:44:29 -0500

    fix build stuff

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rqt_image_view'],
     package_dir={'': 'src'},
-    scripts=['scripts/rqt_image_view']
+    scripts=['scripts/rqt_image_view'],
+    entry_points={
+        'console_scripts': [
+            'rqt_image_view = scripts.rqt_image_view:main',
+        ],
+    },
 )
 
 setup(**d)
